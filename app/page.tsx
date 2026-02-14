960eaa3f67fef1114a109d306d7e90139e41041d
hoxz <hooooxz@gmail.com>
2026-02-14 13:19:13 +0800

    chore: remove unused isMobile state from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,8 @@
 'use client';
 
-import { useState, useEffect } from 'react';
 import Scene from './components/Scene';
 
 export default function Home() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const checkMobile = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-    checkMobile();
-    window.addEventListener('resize', checkMobile);
-    return () => window.removeEventListener('resize', checkMobile);
-  }, []);
-
   return (
     <main style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
       <Scene />
